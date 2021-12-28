5a49576fa4366e5d3e069853420d408558f4fdc9
Srishti-Sahu <srishtisahu2609@gmail.com>
2021-12-28 07:58:09 +0530

    Files added

--- jsTime.js
+++ jsTime.js
@@ -1,2 +1,6 @@
-var d = new Date();
-document.querySelector("#demo").innerHTML = d;
+const d = document.querySelector("#demo");
+setInterval(() => {
+    const date = new Date();
+    let time = ` ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+d.innerText=time}, 1000);
+
